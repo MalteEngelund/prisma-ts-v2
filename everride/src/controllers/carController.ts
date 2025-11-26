e93c7f56a94b5efca1f47e20dc03c39f2cb66fcf
MalteEngelund <malte.engelund@hotmail.com>
2025-11-26 22:30:40 +0100

    CRUD
    tilføjet controller og router til car

---
+++ everride/src/controllers/carController.ts
@@ -0,0 +1,122 @@
+import { Request, Response } from 'express';
+import { prisma } from '../prisma.js';
+
+
+
+export const getRecords = async (req: Request, res: Response) => {
+  try {
+    const data = await prisma.car.findMany({
+      select: {
+        id: true, // maybe?
+        category: true,
+        brand: true,
+        model: true,
+        year: true,
+        price: true,
+        fueltype: true
+      },
+      orderBy: {
+        price: 'asc'
+      }
+    })
+    res.json(data)
+  } catch (error) {
+    console.error(error)
+    res.status(500).send(`DB Fejl: Kunne ikke hente liste af biler`)
+  }
+};
+
+export const getRecord = async (req: Request, res: Response) => {
+  const id = Number(req.params.id)
+
+  if (!id) {
+    return res.status(400).json({ error: 'Id has no value' });
+  }
+
+  try {
+    const data = await prisma.car.findUnique({
+      where: {
+        id
+      }
+    });
+    return res.status(200).json(data);
+  } catch (error) {
+    console.error(error); // Failed request log
+    res.status(500).json({ error: 'Failed to fetch car' });
+  }
+};
+
+
+export const createRecord = async (req: Request, res: Response) => {
+  const { category, brand, model, year, price, fueltype } = req.body;
+
+  if (!category || !brand || !model || !year || !price || !fueltype) {
+    return res.status(400).json({ error: 'Alle felter skal udfyldes' });
+  }
+
+  try {
+    const data = await prisma.car.create({
+      data: {
+        category,
+        brand,
+        model,
+        year: Number(year),
+        price,
+        fueltype
+      }
+    });
+
+    return res.status(201).json(data);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: 'Noget gik galt i serveren' });
+  }
+};
+
+export const updateRecord = async (req: Request, res: Response) => {
+  const id = Number(req.params.id) // Sikrer at id er et tal
+  const { category, brand, model, year, price, fueltype } = req.body // Deconstruerer form body objektet
+
+  if(!id) {
+    return res.status(400).json({ error: 'Id skal have en gyldig værdi' });
+  }
+
+  if(!category || !brand || !model || !year || !price || !fueltype) {
+    return res.status(400).json({ error: 'Alle felter skal udfyldes' });
+  }
+
+  try {
+    const data = await prisma.car.update({
+      where: { id },
+      data: {
+        category,
+        brand,
+        model,
+        year: Number(year),
+        price,
+        fueltype
+      }
+    })
+
+    return res.status(201).json(data);
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: 'Noget gik galt i serveren' });
+  }
+};
+
+export const deleteRecord = async (req: Request, res: Response) => {
+  const id = Number(req.params.id)
+
+  try {
+    await prisma.car.delete({
+      where: { id },
+    });
+
+    res.status(200).json({ message: `Bil nr. ${id} er slettet` });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Kunne ikke slette bilen' });
+  }
+};
---
+++ everride/src/index.ts
@@ -0,0 +1,34 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { userRoutes } from './routes/userRoutes.js';
+import { carRoutes } from './routes/carRoutes.js';
+
+// Indlæs miljøvariabler fra .env (uden at vise logs)
+dotenv.config({ quiet: true });
+
+// Brug port fra .env eller falde tilbage til 3000
+const port = process.env.PORT || 3000;
+
+// Opret express-app
+const app = express();
+
+// Gør det muligt at modtage JSON i requests
+app.use(express.json());
+
+// Gør det muligt at modtage form-data (fx fra formularer)
+app.use(express.urlencoded({ extended: true }));
+
+// Brug vores user-routes under /api/users
+app.use('/api/users', userRoutes);
+
+app.use('/api/cars', carRoutes)
+
+// 404 route
+app.use((req, res) => {
+  res.status(404).send('kunne ikke finde siden du søgt efter')
+})
+
+// Start serveren
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
