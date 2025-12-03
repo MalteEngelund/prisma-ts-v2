2c7e91252a773967808b8a48f8309229ae36214e
MalteEngelund <malte.engelund@hotmail.com>
2025-12-03 13:49:29 +0100

    added Authorize

--- everride/src/routes/authRoutes.ts
+++ everride/src/routes/authRoutes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 import { authenticateToken } from '../middleware/authenticateToken';
 import { getUserProfile } from '../controllers/authController';
+import { authorizeRole } from '../middleware/authorizeRole';
 
 const router = Router();
 
 // Først kører authenticateToken (tjekker om token er gyldig)
 // Hvis token er OK, kører getUserProfile og returnerer brugerens data
 router.get('/authenticate', authenticateToken, getUserProfile);
+
+
+router.get('/authorize', authenticateToken, authorizeRole('ADMIN', 'USER'), getUserProfile);
 
 export { router as authRoutes };
